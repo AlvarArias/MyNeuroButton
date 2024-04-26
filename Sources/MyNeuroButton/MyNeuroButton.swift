e31c0aebacec939414f7089325f32f159cadb933
Alvar Arias <alvararias@gmail.com>
2024-04-26 14:08:51 +0200

    final button

--- Sources/MyNeuroButton/MyNeuroButton.swift
+++ Sources/MyNeuroButton/MyNeuroButton.swift
@@ -1,6 +1,90 @@
-public struct MyNeuroButton {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+/*
+import SwiftUI
+
+public struct MyNeuroButton: View {
+    @State private var isClicked = false
+    public var action: () -> Void
+
+    public init(action: @escaping () -> Void) {
+        self.action = action
+    }
+
+    public var body: some View {
+        Button(action: {
+            self.isClicked.toggle()
+            if self.isClicked { self.action() }
+        }) {
+            Text("Click me")
+        }
     }
 }
+*/
+
+import SwiftUI
+
+public struct MyNeuroButton: View {
+    
+    @State private var isClick = false
+   
+    let icon: String
+    let icon_full: String
+    let fillColor: Color
+    let button_height: CGFloat
+    let button_width: CGFloat
+    let action: () -> Void
+    
+    public init(icon: String, icon_full: String, fillColor: Color, button_height: CGFloat, button_width: CGFloat, action: @escaping () -> Void) {
+        self.icon = icon
+        self.icon_full = icon_full
+        self.fillColor = fillColor
+        self.button_height = button_height
+        self.button_width = button_width
+        self.action = action
+    }
+    
+    public var body: some View {
+        ZStack {
+            Color.offWhite
+            Button(action: {
+                isClick.toggle()
+                if isClick { action()}
+            }) {
+                if isClick {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(fillColor)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.gray, lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: 2, y: 2)
+                                    .mask(Rectangle().fill(LinearGradient(gradient: Gradient(colors: [Color.black, Color.clear]), startPoint: .top, endPoint: .bottom)))
+                            )
+                            .padding()
+                        Image(systemName: icon_full)
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: button_width, height: button_height)
+                } else {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.offWhite)
+                            .shadow(color: Color.black.opacity(0.3), radius: 10, x: 10, y: 10)
+                            .shadow(color: Color.white.opacity(0.8), radius: 10, x: -5, y: -5)
+                            .padding()
+                        Image(systemName: icon)
+                            .foregroundColor(.gray)
+                    }
+                    .frame(width: button_width, height: button_height)
+                }
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+extension Color {
+    static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
+}
+
--- Tests/MyNeuroButtonTests/MyNeuroButtonTests.swift
+++ Tests/MyNeuroButtonTests/MyNeuroButtonTests.swift
@@ -1,11 +1,33 @@
+/*
 import XCTest
 @testable import MyNeuroButton
 
 final class MyNeuroButtonTests: XCTestCase {
-    func testExample() throws {
+   /* func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyNeuroButton().text, "Hello, World!")
+        XCTAssertEqual(MyNeuroButton)., )
+    }*/
+  
+}
+*/
+import SwiftUI
+import XCTest
+@testable import MyNeuroButton
+
+final class MyNeuroBUttons: XCTestCase {
+    func testButtonAction() {
+        var actionWasCalled = false
+
+        let button = MyNeuroButton(icon: "star", icon_full: "star.full", fillColor: Color.blue, button_height: 100, button_width: 100, action: {
+            actionWasCalled = true
+        })
+
+        button.action()
+
+        XCTAssertTrue(actionWasCalled, "The button's action should be called.")
     }
 }
+
+
